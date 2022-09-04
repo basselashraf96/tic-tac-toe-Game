4ab4cf33c804f82d9801c0a25ba52903dd56e475
unknown <bassel.ashraf.gharib@gmail.com>
2022-09-04 16:37:56 +0200

    adding more options

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const allBlocks = document.querySelectorAll('.block')
 const btn = document.querySelector('#btn')
 const header = document.querySelector('header')
+const modes = document.querySelectorAll('.mode')
+const container = document.querySelector('#container')
+const chngMode = document.querySelector('#change-mode')
 
 const b1 = document.querySelector('#a')
 const b2 = document.querySelector('#b')
@@ -13,32 +16,137 @@ const b8 = document.querySelector('#h')
 const b9 = document.querySelector('#i')
 
 let X_turn = true
-let Y_turn = false
+let O_turn = false
 let winnerFlag = false
+let f_1v1 = false
+let f_1vBot = false
 
-for (let block of allBlocks) {
-    block.addEventListener('click', (e) => {
-        if (!X_turn && block.innerText === '' && winnerFlag === false) {
-            block.innerText = 'O'
-            X_turn = true
-            Y_turn = false
-        } else if (!Y_turn && block.innerText === '' && winnerFlag === false) {
-            block.innerText = 'X'
-            X_turn = false
-            Y_turn = true
-        }
-        checkWinner();
-    }, )
-}
-for (let block of allBlocks) {
+const emptyBlocksArr = []
 
-    btn.addEventListener('click', (e) => {
+container.style.display = 'none'
+btn.style.display = 'none'
+chngMode.style.display = 'none'
+
+chngMode.addEventListener('click', () => {
+    location.reload()
+})
+
+chooseMode();
+
+
+btn.addEventListener('click', (e) => {
+    for (let block of allBlocks) {
+        X_turn = true
+        O_turn = false
         block.innerText = ''
         header.innerText = 'X-O GAME'
         header.style.color = '#ffff'
         winnerFlag = false
-    })
+
+    }
+    if (f_1v1 === true) {
+        play_1v1();
+    } else if (f_1vBot === true) {
+        play_1vBot()
+    }
+
+})
+
+
+
+
+
+
+
+
+
+/********************************************************************************************************************************************************************/
+
+
+function chooseMode() {
+    for (let mode of modes) {
+        mode.addEventListener('click', (e) => {
+            if (mode.innerText === '1 vs 1') {
+                f_1v1 = true;
+                f_1vBot = false;
+                container.style.display = 'grid'
+                modes[0].style.display = 'none'
+                modes[1].style.display = 'none'
+                btn.style.display = 'block'
+                chngMode.style.display = 'block'
+                play_1v1()
+            } else {
+                f_1vBot = true
+                f_1v1 = false
+                container.style.display = 'grid'
+                modes[0].style.display = 'none'
+                modes[1].style.display = 'none'
+                btn.style.display = 'block'
+                chngMode.style.display = 'block'
+                play_1vBot()
+            }
+        })
+    }
 }
+
+function play_1v1() {
+
+    for (let block of allBlocks) {
+        block.addEventListener('click', (e) => {
+            if (!X_turn && block.innerText === '' && winnerFlag === false) {
+                block.innerText = 'O'
+                X_turn = true
+                O_turn = false
+            } else if (!O_turn && block.innerText === '' && winnerFlag === false) {
+                block.innerText = 'X'
+                X_turn = false
+                O_turn = true
+            }
+            checkWinner();
+        }, )
+    }
+}
+
+
+function play_1vBot() {
+    let counter = 0;
+    let randomB = randomBlock()
+    for (let block of allBlocks) {
+        block.addEventListener('click', (e) => {
+
+            if (block.innerText === '' && winnerFlag === false) {
+                block.innerText = 'X'
+                counter = 0;
+                checkWinner();
+                while (1 && winnerFlag === false) {
+
+                    if (allBlocks[randomB].innerText === '') {
+                        allBlocks[randomB].innerText = 'O'
+
+                        break;
+                    } else if (counter === 3000) {
+
+                        break;
+                    }
+                    randomB = randomBlock()
+                    counter++;
+
+                }
+
+            }
+
+            checkWinner();
+        }, )
+    }
+
+}
+
+let randomBlock = function() {
+
+    return Math.floor(Math.random() * 9);
+}
+
+
 
 function checkWinner() {
     if (b1.innerText === 'X' && b2.innerText === 'X' && b3.innerText === 'X') {
